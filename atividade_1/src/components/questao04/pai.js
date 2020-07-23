51ebee73182200dc0af05b4f21d0d1215b78eae1
Cristiano <cristianojr@alu.ufc.br>
2020-07-22 23:38:49 -0300

    featured: bootstrap

--- atividade_1/src/components/questao04/pai.js
+++ atividade_1/src/components/questao04/pai.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 
-// importar e instalar o bootstrap
-// cards
+import { Card } from 'react-bootstrap' // cards
+import { ListGroup } from 'react-bootstrap';
 
 export default class Index extends Component {
   render() {
     return (
-      <>
-        <h1>Questão {this.props.id}</h1>
-        <p>Nome: {this.props.name}</p>
-        <p>Curso: {this.props.course}</p>
-        <p>Cidade origem: {this.props.city}</p>
-      </>
+      <Card>
+        <Card.Header as="h1">Questão {this.props.id}</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item as="p">Nome: {this.props.name}</ListGroup.Item>
+          <ListGroup.Item as="p">Curso: {this.props.course}</ListGroup.Item>
+          <ListGroup.Item as="p">Cidade origem: {this.props.city}</ListGroup.Item>
+        </ListGroup>
+      </Card>
     )
   }
 }
+
+/**
+ * <h1>Questão {this.props.id}</h1>
+  <p>Nome: {this.props.name}</p>
+  <p>Curso: {this.props.course}</p>
+  <p>Cidade origem: {this.props.city}</p>
+ */
