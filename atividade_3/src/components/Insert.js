f70ecf9887d7e35103d8cfb99c19bc86446ad743
Cristiano <cristianojr@alu.ufc.br>
2020-08-12 10:45:31 -0300

    feat: atividade_3

---
+++ atividade_3/src/components/Insert.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+export default class Insert extends Component{
+    constructor(props){
+        super(props)
+        this.state = {nome:'', curso:'', capacidade:''}
+
+        this.setNome = this.setNome.bind(this)
+        this.setCurso = this.setCurso.bind(this)
+        this.setCapacidade = this.setCapacidade.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    setNome(e){
+        this.setState({nome:e.target.value})
+    }
+    setCurso(e){
+        this.setState({curso:e.target.value})
+    }
+    setCapacidade(e){
+        this.setState({capacidade:e.target.value})
+    }
+
+    onSubmit(e){
+        e.preventDefault()
+        const novaDisciplina = {nome:this.state.nome,
+                                curso:this.state.curso,
+                                capacidade:this.state.capacidade}
+        axios.post('http://localhost:3001/disciplina', novaDisciplina)
+        .then(
+            (res)=>{
+                console.log('Disciplina Inserida')
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+        this.setState({nome:'',curso:'',capacidade:''})
+    }
+    render(){
+        return(
+            <div style={{marginTop:10}}>
+                <h3>Inserir disciplina</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Nome da disciplina:</label>
+                        <input type="text" className="form-control" value={this.state.nome} onChange={this.setNome}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Curso:</label>
+                        <input type="text" className="form-control" value={this.state.curso} onChange={this.setCurso}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Capacidade:</label>
+                        <input type="number" className="form-control" value={this.state.capacidade} onChange={this.setCapacidade}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Adicionar" className="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
